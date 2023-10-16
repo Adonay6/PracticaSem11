a3dc14abfbc843a51a9acd1b030eb454c896bd6c
Adonay6 <125324486+Adonay6@users.noreply.github.com>
2023-10-15 21:42:03 -0600

    Add files via upload

---
+++ ejercicio11Ado/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio11Ado
+{
+    public class Context : DbContext
+    {
+        public DbSet<Estudiante> Estudiante { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-NFDMETJ; Database= Progra2;Trusted_Connection=SSPI;MultipleActiveResultSets=true;Trust Server Certificate=true;");
+        }
+    }
+    }
